1d1286875b35640568a666b7e0c02e2b8f1d2a44
Kamil Balda <baldakamil@gmail.com>
2018-07-04 13:32:38 +0200

    data binding, using v-bind and v-html to bind whole HTML tags

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el: '#vue-app',
     data: {
         name: 'Kamil',
-        job: 'front-end developer'
+        job: 'front-end developer',
+        website: 'https://www.wp.pl',
+        websiteTag: '<a href="https://www.wp.pl">Stronka wp.pl</a>'
     },
     methods: {
         greet: function(time){
