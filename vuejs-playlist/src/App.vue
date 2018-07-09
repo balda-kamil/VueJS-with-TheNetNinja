fe886808dcb289aff8d2759f1dfeb4fbbb1e6918
Kamil Balda <baldakamil@gmail.com>
2018-07-09 15:23:48 +0200

    scoped css

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,24 +1,27 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <ninjas></ninjas>
-  </div>
+    <div>
+        <h1>{{ title }}</h1>
+        <ninjas></ninjas>
+    </div>
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue'
-
+// Imports
+import Ninjas from './Ninjas.vue';
 export default {
-  components: {
-    'ninjas': Ninjas
-  },
-   data () {
-    return {
-      title: "Some App"
+    components: {
+        'ninjas': Ninjas
+    },
+    data () {
+        return {
+          title: 'Ninja App'
+        }
     }
-  }
-} 
+}
 </script>
 
 <style>
+h1{
+    color: purple;
+}
 </style>
--- vuejs-playlist/src/Ninjas.vue
+++ vuejs-playlist/src/Ninjas.vue
@@ -1,20 +1,28 @@
 <template>
-  <div>
-    <ul>
-        <li v-for="ninja in ninjas">{{ ninja }}</li>
-    </ul>
-  </div>
+    <div>
+        <h1>List of ninjas</h1>
+        <ul>
+            <li v-for="ninja in ninjas">{{ ninja }}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-   data () {
-    return {
-      ninjas: ['Kamil', 'Bartek', 'Yoshi', 'PatiKoszi']
+    data () {
+        return {
+          ninjas: ['Yoshi', 'Mario', 'Ryu']
+        }
     }
-  }
-} 
+}
 </script>
 
-<style>
+<style scoped>
+h1{
+    color: green;
+}
+li{
+    list-style-type: none;
+    padding: 6px;
+}
 </style>
