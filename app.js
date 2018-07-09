e152b8124a0a6e430aab1a91da70713ccb43813d
Kamil Balda <baldakamil@gmail.com>
2018-07-09 11:39:32 +0200

    Multiple Vue Instances, comunication beetwen instances

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-new Vue({
-    el: '#vue-app',
+var one = new Vue({
+    el: '#vue-app-one',
     data: {
-        name: 'Jack',
-        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-        ninjas: [
-            { name: 'Ryu', age: 25 },
-            { name: 'Yoshi', age: 35 },
-            { name: 'Ken', age: 55 }
-        ]
+        title: "Vue App One"
     },
     methods: {
 
     },
     computed: {
-       
+       greet: function(){
+           return "Hello from app one!"
+       }
     }
 });
+
+var two = new Vue({
+    el: '#vue-app-two',
+    data: {
+        title: "Vue App Two"
+    },
+    methods: {
+        changeTitle: function(){
+            one.title = "Title has been changed by another VUE INSTANCE!!"
+        }
+    },
+    computed: {
+        greet: function(){
+            return "Yoo, App Two Here!!"
+        }
+    }
+});
+
+two.title = "change from outside!"
