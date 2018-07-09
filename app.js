dc2e811689097960669b73942f29e7ccd0958922
Kamil Balda <baldakamil@gmail.com>
2018-07-09 13:08:27 +0200

    test

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
 new Vue({
-    el: '#vue-app',
-    data: {
-        output: "Your Fav food"
-    },
-    methods: {
-        readRefs: function(){
-            console.log(this.$refs);
-            this.output = this.$refs.input.value;
-        }
-    }
+   
 })
