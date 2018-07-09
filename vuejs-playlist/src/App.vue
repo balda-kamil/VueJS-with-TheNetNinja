22648c5dc848a8f083905edd135e41269ff7baaf
Kamil Balda <baldakamil@gmail.com>
2018-07-09 14:20:37 +0200

    Nesting Components

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p>{{ greeting() }}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+import Ninjas from './Ninjas.vue'
+
 export default {
+  components: {
+    'ninjas': Ninjas
+  },
    data () {
     return {
-      title: "My first Vue file, łooo"
-    }
-  },
-  methods: {
-    greeting: function(){
-      return 'heey cowboy!';
+      title: "Some App"
     }
   }
-}
+} 
 </script>
 
 <style>
-
 </style>
