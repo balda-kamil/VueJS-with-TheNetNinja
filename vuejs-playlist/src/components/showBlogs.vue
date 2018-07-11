ee52f3bd72ad91c7480847861352d0f266b2d163
Kamil Balda <baldakamil@gmail.com>
2018-07-11 15:13:23 +0200

    Custom Directives, values

--- vuejs-playlist/src/components/showBlogs.vue
+++ vuejs-playlist/src/components/showBlogs.vue
@@ -1,8 +1,8 @@
 <template>
-    <div id="show-blogs">
+    <div v-theme:column="'narrow'" id="show-blogs">
         <h1>All Blog Articles</h1>
         <div v-for="blog in blogs" class="single-blog">
-            <h1> {{ blog.title }} </h1>
+            <h2 v-rainbow> {{ blog.title }} </h2>
             <article> {{ blog.body }} </article>
         </div>
        
