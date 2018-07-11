6b396d1861dea2e164d3bcf10eefbee72b991884
Kamil Balda <baldakamil@gmail.com>
2018-07-11 15:37:15 +0200

    Custom Search Filter

--- vuejs-playlist/src/components/showBlogs.vue
+++ vuejs-playlist/src/components/showBlogs.vue
@@ -1,7 +1,8 @@
 <template>
     <div v-theme:column="'wide'" id="show-blogs">
         <h1>All Blog Articles</h1>
-        <div v-for="blog in blogs" class="single-blog">
+        <input type="text" v-model="search" placeholder="search..."/>
+        <div v-for="blog in filteredBlogs" class="single-blog">
             <h2 v-rainbow> {{ blog.title | to-uppercase }} </h2>
             <article> {{ blog.body | snippet }} </article>
         </div>    
@@ -15,6 +16,7 @@ export default {
     data () {
         return {
            blogs: [],
+           search: ''
         }
     },
     methods: {
@@ -24,6 +26,13 @@ export default {
         this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
             this.blogs = data.body.slice(0,10)
         })
+    },
+    computed: {
+        filteredBlogs: function(){
+            return this.blogs.filter((blog) => {
+                return blog.title.match(this.search);
+            })
+        }
     }
 }
 </script>
