6a40d98fb1d6378e2200456d2ba3b0cf30de8bfe
Kamil Balda <baldakamil@gmail.com>
2018-07-11 15:27:50 +0200

    filters are awasome

--- vuejs-playlist/src/components/showBlogs.vue
+++ vuejs-playlist/src/components/showBlogs.vue
@@ -1,11 +1,10 @@
 <template>
-    <div v-theme:column="'narrow'" id="show-blogs">
+    <div v-theme:column="'wide'" id="show-blogs">
         <h1>All Blog Articles</h1>
         <div v-for="blog in blogs" class="single-blog">
-            <h2 v-rainbow> {{ blog.title }} </h2>
-            <article> {{ blog.body }} </article>
-        </div>
-       
+            <h2 v-rainbow> {{ blog.title | to-uppercase }} </h2>
+            <article> {{ blog.body | snippet }} </article>
+        </div>    
     </div>
 </template>
 
