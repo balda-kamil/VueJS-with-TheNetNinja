ab9839cd6d7731e4c39d25024cc1f8461723df80
Kamil Balda <baldakamil@gmail.com>
2018-07-11 13:19:55 +0200

    Checkbox Binding

--- vuejs-playlist/src/components/addBlog.vue
+++ vuejs-playlist/src/components/addBlog.vue
@@ -6,12 +6,27 @@
             <input type="text" v-model.lazy="blog.title" required/>
             <label>Blog Content:</label>
             <textarea v-model.lazy="blog.content"></textarea>
+            <div id="checkboxes">
+                <label>Ninjas</label>
+                    <input type="checkbox" value="ninjas" v-model="blog.categories"/>
+                <label>Wizards</label>
+                    <input type="checkbox" value="wizards" v-model="blog.categories"/>
+                <label>Heroes</label>
+                    <input type="checkbox" value="heroes" v-model="blog.categories"/>
+                <label>Gods</label>
+                    <input type="checkbox" value="gods" v-model="blog.categories"/>
+
+            </div>
         </form>
         <div id="preview">
             <h3>Preview Blog</h3>
             <p>Blog title: {{ blog.title }}</p>
             <p>Blog content: </p>
             <p>{{ blog.content }}</p>
+            <p>Blog Categories:</p>
+            <ul>
+                <li v-for="category in blog.categories">{{ category }}</li>
+            </ul>
         </div>
     </div>
 </template>
@@ -26,7 +41,8 @@ export default {
         return {
             blog: {
                 title: "",
-                content: ""
+                content: "",
+                categories: []
             }
         }
     },
@@ -59,5 +75,14 @@ input[type="text"], textarea{
 }
 h3{
     margin-top: 10px;
+}
+
+#checkboxes input{
+    display: inline-block;
+    margin-right: 10px;
+}
+
+#checkboxes label{
+    display: inline-block;
 }
 </style>
