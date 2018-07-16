c6c0fe3496f7cd6936527e476c1e54d05f77fd2c
Kamil Balda <baldakamil@gmail.com>
2018-07-16 14:13:46 +0200

    setting up routing'

--- vuejs-playlist/src/main.js
+++ vuejs-playlist/src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 // Use vue-resource package
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
+const router = new VueRouter({
+  routes: Routes
+});
 // Filters
 /*
 Vue.filter('to-uppercase', function(value){
@@ -13,5 +19,6 @@ Vue.filter('to-uppercase', function(value){
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 })
