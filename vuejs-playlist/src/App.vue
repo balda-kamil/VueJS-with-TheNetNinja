81831c252a6688357b7bda4e5280d78e5a95ceb9
balda-kamil <baldakamil@gmail.com>
2018-08-17 08:25:50 +0200

    header and some style added

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,6 +1,7 @@
 
 <template>
     <div>
+        <app-header></app-header>
         <router-view></router-view>
     </div>
 </template>
@@ -10,12 +11,14 @@
 import showBlogs from './components/showBlogs.vue';
 import listBlogs from './components/listBlogs.vue';
 import addBlog from './components/addBlog.vue';
+import header from './components/header.vue';
 
 export default {
     components: {
         'show-blogs': showBlogs,
         'add-blog': addBlog,
-        'list-blogs': listBlogs
+        'list-blogs': listBlogs,
+        'app-header': header
     },
     data () {
         return {
